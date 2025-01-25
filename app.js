86dd6057c79c4b92af2b1a1b9bfcc58986d5b167
Harshit G <harshitguptahg57@gmail.com>
2025-01-25 15:45:57 +0530

    changes for product

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import express from "express";
 import Routes from "./routes/routes.js";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
+import cors from "cors";
 
 export class App {
-
   constructor() {
-
     this.app = express();
-    this.app.use(bodyParser.json({limit: '1000mb'}));
-    this.app.use(bodyParser.urlencoded({limit: '1000mb', extended: true}));
-    this.app.use(express.json({ limit:  '1000mb' }));
-    this.app.use(express.urlencoded({ limit:  '1000mb', extended: true }));
+    this.app.use(bodyParser.json({ limit: "1000mb" }));
+    this.app.use(bodyParser.urlencoded({ limit: "1000mb", extended: true }));
+    this.app.use(express.json({ limit: "1000mb" }));
+    this.app.use(express.urlencoded({ limit: "1000mb", extended: true }));
+    this.app.use(cors());
     Routes.init(this);
   }
 }
--- controller/getDivine.js
+++ controller/getDivine.js
@@ -1,91 +1,62 @@
-
 import { pool } from "../db.js";
 
 class GetDivineController {
-    async getBlogCategories(req, res) {
-        let [data] = await pool.execute(
-          `SELECT * FROM BlogCategories;`
-        );
-        res.json(data);
-      }
-      async getBlogTags(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM BlogTags;`
-          );
-          res.json(data);
-      }
-      async getBlogs(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM Blogs;`
-          );
-          res.json(data);
-      }
-      async getCitations(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM Citations;`
-          );
-          res.json(data);
-      }
-      async getCourseCategories(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM CourseCategories;`
-          );
-          res.json(data);
-      }
-      async getCourseTags(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM CourseTags;`
-          );
-          res.json(data);
-      }
-      async getSpiritualities(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM Spiritualities;`
-          );
-          res.json(data);
-      }
-      async getSpiritualityTags(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM SpiritualityTags;`
-          );
-          res.json(data);
-      }
-      async getSpiritualityCategories(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM SpiritualityCategories;`
-          );
-          res.json(data);
-      }
-      async getTestimonials(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM Testimonials;`
-          );
-          res.json(data);
-      }
-      async getWebStories(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM WebStories;`
-          );
-          res.json(data);
-      }
-      async getWebStoryCategories(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM WebStoryCategories;`
-          );
-          res.json(data);
-      }
-      async getWebStoryTags(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM WebStoryTags;`
-          );
-          res.json(data);
-      }
-      async getCourses(req, res) {
-          let [data] = await pool.execute(
-          `SELECT * FROM courses;`
-          );
-          res.json(data);
-      }
+  async getBlogCategories(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM BlogCategories;`);
+    res.json(data);
+  }
+  async getBlogTags(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM BlogTags;`);
+    res.json(data);
+  }
+  async getBlogs(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM Blogs;`);
+    res.json(data);
+  }
+  async getCitations(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM Citations;`);
+    res.json(data);
+  }
+  async getCourseCategories(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM CourseCategories;`);
+    res.json(data);
+  }
+  async getCourseTags(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM CourseTags;`);
+    res.json(data);
+  }
+  async getSpiritualities(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM Spiritualities;`);
+    res.json(data);
+  }
+  async getSpiritualityTags(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM SpiritualityTags;`);
+    res.json(data);
+  }
+  async getSpiritualityCategories(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM SpiritualityCategories;`);
+    res.json(data);
+  }
+  async getTestimonials(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM Testimonials;`);
+    res.json(data);
+  }
+  async getWebStories(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM WebStories;`);
+    res.json(data);
+  }
+  async getWebStoryCategories(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM WebStoryCategories;`);
+    res.json(data);
+  }
+  async getWebStoryTags(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM WebStoryTags;`);
+    res.json(data);
+  }
+  async getCourses(req, res) {
+    let [data] = await pool.execute(`SELECT * FROM Products;`);
+    res.json(data);
+  }
 }
 
 export default new GetDivineController();
