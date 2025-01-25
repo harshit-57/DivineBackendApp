2cc0de1d8528db8b8df82e4edd72362c5b5b737a
Harshit G <harshitguptahg57@gmail.com>
2025-01-25 17:21:51 +0530

    blog list api changes with pagination and filter

--- controller/getDivine.js
+++ controller/getDivine.js
@@ -2,7 +2,13 @@ import { pool } from "../db.js";
 
 class GetDivineController {
   async getBlogCategories(req, res) {
-    let [data] = await pool.execute(`SELECT * FROM BlogCategories;`);
+    let [data] = await pool.execute(
+      `SELECT ct.* FROM BlogCategories as ct
+        JOIN BlogMappingCategory as bgct ON ct.id = bgct.BlogCategoryId
+        JOIN Blogs as bg ON bgct.BlogId = bg.id
+        GROUP BY ct.id
+        ORDER BY ct.Name ASC;`
+    );
     res.json(data);
   }
   async getBlogTags(req, res) {
@@ -10,8 +16,44 @@ class GetDivineController {
     res.json(data);
   }
   async getBlogs(req, res) {
-    let [data] = await pool.execute(`SELECT * FROM Blogs;`);
-    res.json(data);
+    const payload = req.query;
+    let filters = [];
+
+    const offset = ((payload?.page || 1) - 1) * (payload?.pageSize || 10);
+    const limit = payload?.pageSize || 10;
+    const sort = payload?.sort || "DESC";
+    const sortBy = payload?.sortBy || "bg.PublishedOn";
+
+    if (payload?.search) {
+      filters.push(`bg.Title LIKE "%${payload.search}%"`);
+    }
+
+    if (payload.category) {
+      filters.push(`ct.id = "${payload.category}"`);
+    }
+
+    let [data] = await pool.execute(
+      `SELECT bg.* , ct.Name AS CategoryName, ct.id AS CategoryId, ct.slug AS CategorySlug
+        FROM Blogs as bg
+        JOIN BlogMappingCategory as bgct ON bg.id = bgct.BlogId
+        JOIN BlogCategories as ct ON bgct.BlogCategoryId = ct.id
+        ${filters.length > 0 ? `WHERE ${filters.join(" AND ")}` : ""}
+        ORDER BY ${sortBy} ${sort} 
+        LIMIT ${limit} OFFSET ${offset} ;`
+    );
+    let [count] = await pool.execute(
+      `SELECT COUNT(*) as total FROM Blogs as bg
+        JOIN BlogMappingCategory as bgct ON bg.id = bgct.BlogId
+        JOIN BlogCategories as ct ON bgct.BlogCategoryId = ct.id
+        ${filters.length > 0 ? `WHERE ${filters.join(" AND ")}` : ""};`
+    );
+
+    const total = count[0].total;
+    res.json({
+      success: 1,
+      data,
+      total,
+    });
   }
   async getCitations(req, res) {
     let [data] = await pool.execute(`SELECT * FROM Citations;`);
