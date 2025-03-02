bf85e3f876e36a82b5339b5639e4df90c516c32b
Harshit G <harshitguptahg57@gmail.com>
2025-03-02 19:25:39 +0530

    changes for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: 'classified-website-api',
-      script: 'npm',
-      args: 'run start',
+      name: "divine-backend",
+      script: "index.js",
+      args: "run start",
       watch: false,
       env: {
-        NODE_ENV: 'production',
-        PORT: 3306 // specify the port for the development environment
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 3306 // specify the port for the production environment
+        NODE_ENV: "local",
+        PORT: 4200,
       },
       env_development: {
-        NODE_ENV: 'development',
-        PORT: 3306 // specify the port for the production environment
-      }
-    }
-  ]
+        NODE_ENV: "development",
+        PORT: 4200,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 8000,
+      },
+    },
+  ],
 };
-
